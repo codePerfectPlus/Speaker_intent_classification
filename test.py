9dd17871926e5a456db754c454bd1f17d4136be5
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-06-25 15:27:40 +0530

    increasing threshold

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import requests
 url = "http://34.141.241.48:5000/api/v1/authenticate"
 
 payload={'username': 'deepak008'}
+
 files=[
   ('file',('voice_login.wav',open('C:/Users/inspiron/Documents/GitHub/voice_auth_using_cnn/data/wav/Deepak11/voice_login.wav','rb'),'audio/wav'))
 ]
