9c7cb92c84540977f2a2db69db7049f7f69945de
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2021-06-28 19:06:37 +0530

    refactoring the code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 if __name__ == '__main__':
     if sys.argv[1] == "local":
         app.run()
-    app.run(host='0.0.0.0', port=5000)
+    if sys.argv[1] == "run":
+        app.run(host='0.0.0.0', port=5000)
 
 #gunicorn main:app --bind 0.0.0.0:5000
